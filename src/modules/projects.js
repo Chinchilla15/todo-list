e19ab026bed426e9ec02ee3f52605dbd0a71ddaf
Alessandro Chinchilla <ale030401@gmail.com>
2024-01-03 13:01:29 -0600

    add: function to add todo to project

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,7 +1,8 @@
-import ToDo from "./todos";
-
-function Project(name, projectToDos){
+export default function Project(name){
     this.name = name;
-    this.projectToDos = projectToDos
-    projectToDos = [];
-}
+    this.projectToDos = [];
+};
+
+Project.prototype.addTodo = function(todo){
+    this.projectToDos.push(todo);
+};
