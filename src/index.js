5fc01a19d9954281b284cb76ee0daba73271adb9
Alessandro Chinchilla <ale030401@gmail.com>
2024-01-03 18:42:17 -0600

    Add: example content in console

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import { createProject, createTodo, addTodoToProject } from "./modules/logic";
+import { createProject, createTodo, projectList } from "./modules/logic";
 
 const schoolProject = createProject("School");
+const carProject = createProject("Car")
 
 const newTodo = createTodo("Do Homework", "School", "05-01-24", "low", "math homework");
 const newTodo1 = createTodo("Study for Exam", "School", "05-01-25", "medium", "history exam");
+const schoolTodo = createTodo("Test","School","05-01-25", "important", "science exam")
 
-addTodoToProject(newTodo, schoolProject);
-addTodoToProject(newTodo1, schoolProject);
+const hometodo = createTodo("Do Homework", "Home", "05-01-24", "low", "math homework")
 
-console.log(schoolProject);
+const code = createTodo("Create a todo app", "Programming", "15-01-24", "Important", "Project from TOP")
+
+const none = createTodo("No project", "", "Sometime", "meh", "test")
+
+const carTodo = createTodo("Repair car", "Car", "08-02-24", "High", "Use new oil")
+
+console.log(projectList)
