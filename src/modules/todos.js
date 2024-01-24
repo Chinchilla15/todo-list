d2ace96673bd9a0630bfcd175d4b037234325357
Alessandro Chinchilla <ale030401@gmail.com>
2024-01-23 18:39:06 -0600

    Add completed parameter to check task completion state

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,6 +1,7 @@
-export default function ToDo(title, project, dueDate, priority){
+export default function ToDo(title, project, dueDate, priority, completed = false){
     this.title = title;
     this.project = project;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.completed = completed;
 };
